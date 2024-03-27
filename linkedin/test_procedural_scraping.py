e15a42643c24befa2cba1aa839293f8890a962c2
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2024-03-27 14:50:31 -0300

    feat: adicionado funções para garantir o carregamento de elementos e fechar modais

--- linkedin/test_procedural_scraping.py
+++ linkedin/test_procedural_scraping.py
@@ -56,6 +56,19 @@ driver.find_element(by=By.XPATH, value=xpath_login).send_keys(username)
 driver.find_element(by=By.XPATH, value=xpath_password).send_keys(password)
 driver.find_element(by=By.XPATH, value=xpath_submit).click()
 
+# minimize chat if open
+xpath_btn_close_chat = "//*[@data-test-icon='chevron-down-small']"
+                        # "//section[contains(@class, 'org-analytics-export-by-date__container')]"
+try:
+    driver.find_element(by=By.XPATH, value=xpath_btn_close_chat).click()
+    print('Detectado chat aberto, fechando para evitar erros!')
+
+except Exception as e:
+    if "Message: no such element: Unable to locate element" in str(e):
+        pass
+    else:
+        print(str(e))
+
 urls = [
     f"https://www.linkedin.com/company/{company_code}/admin/analytics/updates/",
     f"https://www.linkedin.com/company/{company_code}/admin/analytics/visitors/",
@@ -64,17 +77,24 @@ urls = [
 ]
 
 for url in urls:
-
     print("Acessando url de", url.split("/")[-2])
 
     driver.get(url)
-    time.sleep(1)
 
     xpath_section_export = (
         "//section[contains(@class, 'org-analytics-export-by-date__container')]"
     )
 
-    section_export = driver.find_element(by=By.XPATH, value=xpath_section_export)
+    section_export = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, xpath_section_export))
+    )
+    
+    try:
+        WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.CLASS_NAME,"artdeco-modal__dismiss"))).click()
+    
+    except Exception as e:
+        modal = False
 
     option_buttons = section_export.find_elements(by=By.TAG_NAME, value="button")
 
@@ -86,7 +106,9 @@ for url in urls:
     button_dates.click()
 
     xpath_ul_date_options = "//ul[contains(@class, 'member-analytics-addon-daterange-picker__daterange-options')]"
-    ul_date_options = driver.find_element(by=By.XPATH, value=xpath_ul_date_options)
+    ul_date_options = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, xpath_ul_date_options))
+    )
 
     d15, d30, d90, d365, custom_date = ul_date_options.find_elements(
         by=By.TAG_NAME, value="li"
@@ -97,10 +119,10 @@ for url in urls:
 
     button_export.click()
 
-    time.sleep(2)
     button_export_modal = driver.find_element(
         by=By.CLASS_NAME, value="artdeco-modal__actionbar"
     ).find_elements(by=By.TAG_NAME, value="button")[-1]
     button_export_modal.click()
 
     print("Realizando download de", url.split("/")[-2])
+    time.sleep(2)
